aef825c30375fb6c6a25e43988a56f616320b88c
Naj <153092631+najwarizal@users.noreply.github.com>
2023-12-07 08:51:50 +0800

    script.js

--- script.js
+++ script.js
@@ -1,149 +1,166 @@
-$(document).ready(function () {
+$(document).ready(function() {
     const ball = document.getElementById("ball");
     const paddle = document.getElementById("paddle");
     const box = document.querySelector(".box");
     const scoreTableBody = $("#scoreTable tbody");
 
     let ballX = box.offsetWidth / 2 - ball.offsetWidth / 2;
-    let ballY = box.offsetHeight - paddle.offsetHeight - ball.offsetHeight; // Initially stick to the paddle
-    let ballSpeedX = 0;
-    let ballSpeedY = 0;
-
-    let paddleX = box.offsetWidth / 2 - paddle.offsetWidth / 2;
-    let paddleSpeed = 6;
+    let ballY = box.offsetHeight / 2 - ball.offsetHeight / 2;
+    let ballSpeedX = 4;
+    let ballSpeedY = 6;
 
     let lives = 3;
     let points = 0;
     let gameOver = false;
     let startTime = null;
     let playerName = null;
-    let gameStarted = false;
 
+    // Function to prompt for player name before restarting the game
     function promptForName() {
         playerName = prompt("Enter your name:");
+
+        // If the player entered a name, add it to the table
         if (playerName) {
             addPlayerToTable(playerName);
         }
     }
 
+    // Function to add a player to the table
     function addPlayerToTable(playerName) {
         if (!gameOver) {
             scoreTableBody.append("<tr><td>" + playerName + "</td></tr>");
         }
     }
 
+    // Function to unbind existing click event and bind a new one
     function bindRestartEvent() {
-        $("#restartBtn").off("click").on("click", function () {
+        $("#restartBtn").off("click").on("click", function() {
             promptForName();
             resetGame();
         });
     }
 
+    // Function to reset the game to its initial state
     function resetGame() {
-        ballSpeedX = 0;
-        ballSpeedY = 0;
+        // Reset game variables
         ballX = box.offsetWidth / 2 - ball.offsetWidth / 2;
-        ballY = box.offsetHeight - paddle.offsetHeight - ball.offsetHeight; // Stick to the paddle
-        paddleX = box.offsetWidth / 2 - paddle.offsetWidth / 2;
+        ballY = box.offsetHeight / 2 - ball.offsetHeight / 2;
+        ballSpeedX = 4;
+        ballSpeedY = 6;
         lives = 3;
         points = 0;
         gameOver = false;
         startTime = null;
-        playerName = null;
-        gameStarted = false;
+
+        // Display initial lives, points, and timer
         $("#lives").text("Lives: " + lives);
         $("#points").text("Points: " + points);
         $("#timer").text("Time: 0s");
+
+        // Clear the player name
+        playerName = null;
+
+        // Restart the game loop
         resetBall();
         startGameLoop();
     }
 
+    // Function to start the game loop
     function startGameLoop() {
-        const interval = 1000 / 30;
+        
+		// Set the interval to achieve 30 frames per second
+        const interval = 1000 / 30; // 1000 milliseconds / 30 fps
         setInterval(update, interval);
     }
 
+    // Function to update the game state
     function update() {
+        // Game logic goes here
+
         if (!gameOver) {
-            paddleX = Math.min(Math.max(paddleX, 0), box.offsetWidth - paddle.offsetWidth);
+            // Update ball position
+            ballX += ballSpeedX;
+            ballY += ballSpeedY;
 
-            if (gameStarted) {
-                ballX += ballSpeedX;
-                ballY += ballSpeedY;
-
-                if (ballX + ball.offsetWidth > box.offsetWidth || ballX < 0) {
-                    ballSpeedX = -ballSpeedX;
-                }
-
-                if (ballY + ball.offsetHeight > box.offsetHeight || ballY < 0) {
-                    ballSpeedY = -ballSpeedY;
-                }
-
-                if (
-                    ballX < paddleX + paddle.offsetWidth &&
-                    ballX + ball.offsetWidth > paddleX &&
-                    ballY + ball.offsetHeight > box.offsetHeight - paddle.offsetHeight
-                ) {
-                    ballSpeedY = -ballSpeedY;
-                    incrementPoints();
-
-                    const bounceSound = document.getElementById("bounceSound");
-                    bounceSound.currentTime = 0;
-                    bounceSound.play();
-                }
-
-                if (ballY + ball.offsetHeight > box.offsetHeight) {
-                    lives--;
-
-                    if (lives <= 0) {
-                        gameOver = true;
-                        alert("Game Over!");
-                    } else {
-                        resetBall();
-                    }
-                }
-
-                ball.style.left = ballX + "px";
-                ball.style.top = ballY + "px";
+            // Bounce off the walls
+            if (ballX + ball.offsetWidth > box.offsetWidth || ballX < 0) {
+                ballSpeedX = -ballSpeedX;
             }
 
-            paddle.style.left = paddleX + "px";
-
-            if (gameStarted) {
-                const currentTime = new Date().getTime();
-                const elapsedTime = (currentTime - startTime) / 1000;
-                $("#timer").text("Time: " + elapsedTime.toFixed(2) + "s");
+            // Bounce off the ceiling and floor
+            if (ballY + ball.offsetHeight > box.offsetHeight || ballY < 0) {
+                ballSpeedY = -ballSpeedY;
             }
+
+            // Check if the ball is touching the paddle
+            if (
+                ballX < paddle.offsetLeft + paddle.offsetWidth &&
+                ballX + ball.offsetWidth > paddle.offsetLeft &&
+                ballY + ball.offsetHeight > paddle.offsetTop &&
+                ballY < paddle.offsetTop + paddle.offsetHeight
+            ) {
+                ballSpeedY = -ballSpeedY; // Change the direction of the ball vertically
+                incrementPoints();
+				
+				 // Play the bouncing sound
+				const bounceSound = document.getElementById("bounceSound");
+				bounceSound.currentTime = 0; // Reset the sound to the beginning
+				bounceSound.play();
+            }
+
+            // Check if the ball is out of the box
+            if (ballY + ball.offsetHeight > box.offsetHeight) {
+                lives--;
+
+                if (lives <= 0) {
+                    gameOver = true;
+                    alert("Game Over!"); // You can replace this with your game over logic
+                } else {
+                    resetBall();
+                }
+            }
+
+            // Update ball position
+            ball.style.left = ballX + "px";
+            ball.style.top = ballY + "px";
+
+            // Display remaining lives, points, and timer (in seconds)
+            $("#lives").text("Lives: " + lives);
+            $("#points").text("Points: " + points);
+            const currentTime = new Date().getTime();
+            const elapsedTime = (currentTime - startTime) / 1000; // Convert to seconds
+            $("#timer").text("Time: " + elapsedTime.toFixed(2) + "s");
         }
-    }
+	}
 
+    // Function to move the paddle
     function movePaddle(event) {
-        paddleX = event.clientX - box.getBoundingClientRect().left - paddle.offsetWidth / 2;
-
-        if (!gameStarted) {
-            gameStarted = true;
-            ballSpeedX = 4;
-            ballSpeedY = 4;
-            startTime = new Date().getTime();
-        }
+        // Move the paddle within the box
+        const paddleX = Math.min(Math.max(event.clientX - box.getBoundingClientRect().left - paddle.offsetWidth / 2, 0), box.offsetWidth - paddle.offsetWidth);
+        paddle.style.left = paddleX + "px";
     }
 
+    // Function to reset the ball position
     function resetBall() {
         ballX = box.offsetWidth / 2 - ball.offsetWidth / 2;
-        ballY = box.offsetHeight - paddle.offsetHeight - ball.offsetHeight; // Stick to the paddle
-        gameStarted = false;
+        ballY = box.offsetHeight / 2 - ball.offsetHeight / 2;
+        startTime = new Date().getTime();
     }
 
+    // Function to increment points
     function incrementPoints() {
         points++;
     }
 
+    // Event listener for mouse movement to move the paddle
     $(document).on("mousemove", movePaddle);
 
+    // Call promptForName initially to get player's name
     promptForName();
 
+    // Bind the restart event initially
     bindRestartEvent();
 
+    // Start the game loop initially
     startGameLoop();
-	
 });
